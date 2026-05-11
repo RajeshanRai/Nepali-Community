971ce53afcc1d76a292167f8fc4b7f30308a5f77
Rajeshan Rai <yellowtown85@gmail.com>
2026-05-11 03:36:28 -0700

    final

--- nepali_community/wsgi.py
+++ nepali_community/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for nepali_community project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Add your project directory to sys.path
+path = '/home/rajeshan/nepali_community'
+if path not in sys.path:
+    sys.path.append(path)
 
+# Set Django settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nepali_community.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
